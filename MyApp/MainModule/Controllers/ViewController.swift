511ed87b461dbb090b1f6b480508cb967c992cc3
Vitali Klopau <vitaliklopau@gmail.com>
2023-09-12 01:49:04 +0200

    some code from MainView

--- MyApp/MainModule/Controllers/ViewController.swift
+++ MyApp/MainModule/Controllers/ViewController.swift
@@ -15,11 +15,36 @@ class ViewController: UIViewController {
         imageView.layer.borderColor = UIColor.white.cgColor
         imageView.layer.borderWidth = 5
         imageView.layer.cornerRadius = 50
-        
-        
         imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
     }()
+    
+    private let userNameLabel : UILabel = {
+        let label = UILabel()
+        label.text = "Your name"
+        label.textColor = #colorLiteral(red: 0.3913812339, green: 0.3912524879, blue: 0.3872336149, alpha: 1)
+        label.adjustsFontSizeToFitWidth = true
+        label.minimumScaleFactor = 0.5
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var addWorkoutButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = #colorLiteral(red: 0.9963226914, green: 0.8627573252, blue: 0.4291374087, alpha: 1)
+        button.layer.cornerRadius = 10
+        button.setTitle("Add workout", for: .normal)
+        button.setImage(UIImage(named: "plus"), for: .normal)
+        button.tintColor = #colorLiteral(red: 0.1807179451, green: 0.3627806008, blue: 0.3323107958, alpha: 1)
+        button.imageEdgeInsets = .init(top: 0, left: 20, bottom: 15, right: 0)
+        button.titleEdgeInsets = .init(top: 50, left: -40, bottom: 0, right: 0)
+        button.addTarget(self, action: #selector(addWorkoutButtonTapped), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private let calendarView = CalendarView()
+    
     
     override func viewDidLayoutSubviews() {
         userPhotoImageView.layer.cornerRadius = userPhotoImageView.frame.width / 2
@@ -34,8 +59,15 @@ class ViewController: UIViewController {
     }
     private func setupViews() {
         view.backgroundColor = #colorLiteral(red: 0.9532985091, green: 0.9427116513, blue: 0.9085384011, alpha: 1)
-        
+        view.addSubview(calendarView)
         view.addSubview(userPhotoImageView)
+        view.addSubview(userNameLabel)
+        view.addSubview(addWorkoutButton)
+        
+    }
+    
+    @objc private func addWorkoutButtonTapped() {
+        print("!!!")
     }
 }
 
@@ -49,7 +81,21 @@ extension ViewController {
             userPhotoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0),
             userPhotoImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
             userPhotoImageView.heightAnchor.constraint(equalToConstant: 100),
-            userPhotoImageView.widthAnchor.constraint(equalToConstant: 100)
+            userPhotoImageView.widthAnchor.constraint(equalToConstant: 100),
+            
+            calendarView.topAnchor.constraint(equalTo: userPhotoImageView.centerYAnchor),
+            calendarView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            calendarView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
+            calendarView.heightAnchor.constraint(equalToConstant: 70),
+            
+            userNameLabel.bottomAnchor.constraint(equalTo: calendarView.topAnchor, constant: -10),
+            userNameLabel.leadingAnchor.constraint(equalTo: userPhotoImageView.trailingAnchor, constant: 5),
+            userNameLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
+            
+            addWorkoutButton.topAnchor.constraint(equalTo: calendarView.bottomAnchor, constant: 5),
+            addWorkoutButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            addWorkoutButton.heightAnchor.constraint(equalToConstant: 80),
+            addWorkoutButton.widthAnchor.constraint(equalToConstant: 80)
         ])
     }
 }
